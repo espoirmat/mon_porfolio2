3984e9a81c66ec0de6afb795d9edebf16c956da7
espoir <hpmasevo@gmail.com>
2023-02-21 14:59:38 +0100

    creation de la nav bar

---
+++ src/component/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav_Bar from './Nav_Bar'
+
+function Home() {
+  return (
+    <div>
+      <Nav_Bar/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/component/Nav_Bar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../style/Nav_Bar.css'
+
+function Nav_Bar() {
+  return (
+    <div className='nav_bar'>
+        <h4>Espoir MASEVO</h4>
+        <div className='links'>
+            <a href='#'>Home</a>
+            <a href='#'>A Propos</a>
+            <a href='#'>Compétences</a>
+            <a href='#'>Expériences</a>
+            <a href='#'>Réaliqations</a>
+            <a href='#'>Contact</a>
+            <a href='#'>Home</a>
+        </div>
+    </div>
+  )
+}
+
+export default Nav_Bar
