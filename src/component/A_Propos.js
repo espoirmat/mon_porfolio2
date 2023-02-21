71ab9713bf6d477d7d93c3a85991a82ec5eac22c
espoir <hpmasevo@gmail.com>
2023-02-21 17:46:53 +0100

    telechargement de l'image à propos

--- src/component/A_Propos.js
+++ src/component/A_Propos.js
@@ -12,12 +12,10 @@ function A_Propos() {
         <div className='propos'>
             <img src={image}/>
             <div className='text'>
-            <h1>Salut, je suis Espoir!! </h1>
-            <h2>Dévellopeuse Front End</h2>
-            <p> Ma mission etant développeur Front-end consiste à participer à la création de l'interface utilisateur d'un site ou d'une application web. j'interviens sur tous les éléments apparaissant à l'écran et géré par le navigateur web de l'utilisateur.</p>
-            <div className='button'><a href='#'>Me Contacter </a></div>
+                <p>Je suis une jeune développeuse œuvrant dans le domaine du frontend depuis plus d'un an maintenant. Mon attirance pour la beauté et mode alliée à mon expérience restent mes principaux atouts pour créer au quotidien des sites et applications web attrayants tout en tenant compte de l'expérience utilisateur.</p>
+                <div className='button'><a href='#'>Me Contacter </a></div>
+            </div>
         </div>
-  </div>
     
 
     </div>
