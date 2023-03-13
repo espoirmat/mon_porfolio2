efe96f110c26ede32ebfcef2c813bcbd23c9e4b8
espoir <hpmasevo@gmail.com>
2023-03-13 14:25:30 +0100

    finissions des réalisations

--- src/component/Realisation.js
+++ src/component/Realisation.js
@@ -17,27 +17,52 @@ function Realisation() {
             <div className='real'>
                 <img src={capture7}/>
                 <h2>Site web</h2>
-                <p>Il s'agit de la réproduction de la page d'accueil du site de BlueFontier</p>
+                <p>Il s'agit de la réproduction d'une maquette pour un site web E-commerce de ventes des vêtements pour femmes. </p>
                 <div className='links'><span className='text'>HTML</span><span className='text'>SASS</span><span className='text'>JS</span></div>
                 <a href=''>Regardez!!</a>
             </div>
+
             <div className='real'> 
                 <img src={capture1}/>
+                <h2>Page web</h2>
+                <p>Il s'agit d'une page librairie, pour les amoureux de la culture asiatiques. Dont les mangas, webtoon et manhua </p>
+                <div className='links'><span className='text'>HTML</span><span className='text'>CSS</span><span className='text'>PHP</span></div>
+                <a href=''>Regardez!!</a>
             </div>
             <div className='real'> 
                 <img src={capture2}/>
+                <h2>Site web</h2>
+                <p>IIl s'agit d'un site web réaliser pour RFS consulting</p>
+                <div className='links'><span className='text'>HTML</span><span className='text'>CSS</span></div>
+                <a href=''>Regardez!!</a>
             </div>
             <div className='real'>
                 <img src={capture3}/>
+                <h2>Page web</h2>
+                <p>Il s'agit d'une page web, réaliser en grouper pour l'agence Makambu</p>
+                <div className='links'><span className='text'>HTML</span><span className='text'>CSS</span><span className='text'>JS</span></div>
+                <a href=''>Regardez!!</a>
             </div>
             <div className='real'>
                 <img src={capture4}/>
+                <h2>Page web</h2>
+                <p>Il s'agit de la prèmiere version de mon porfolio  </p>
+                <div className='links'><span className='text'>HTML</span><span className='text'>SASS</span><span className='text'>JS</span></div>
+                <a href=''>Regardez!!</a>
             </div>
             <div className='real'>
                 <img src={capture5}/>
+                <h2>Page web</h2>
+                <p>Il s'agit de la reproduction exacte du design de télephone d'apple</p>
+                <div className='links'><span className='text'>HTML</span><span className='text'>CSS</span><span className='text'>JS</span></div>
+                <a href=''>Regardez!!</a>
             </div>
             <div className='real'>
                 <img src={capture6}/>
+                <h2>Page web</h2>
+                <p>Il s'agit de l'intégration d'un design d'un site des photographies</p>
+                <div className='links'><span className='text'>HTML</span><span className='text'>SAss</span></div>
+                <a href=''>Regardez!!</a>
             </div>  
         </div>
     </div>
